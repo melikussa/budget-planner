6535a40b1bc0723d8ec58faa74260b48faf67be6
Lika <me.likatchavtchavadze@gmail.com>
2024-09-12 13:46:07 +0100

    basic authentification form and some styling

--- client/budget-planner-web/src/components/Auth/AuthForm.js
+++ client/budget-planner-web/src/components/Auth/AuthForm.js
@@ -1,7 +1,50 @@
+import { useState, useRef, useContext } from "react";
+
+import classes from "./AuthForm.module.css";
+
+
 const AuthForm = () => {
-    return <div>
-        <p> Auth form </p>
-    </div>
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    const [isLogin, setIsLogin] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    }
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        // add validation here
+
+        setIsLoading(true);
+        console.log(isLoading + " isss Loading");
+
+        // send request here
+    }
+
+
+
+
+  return (
+    <section className={classes.auth}> 
+        <form>
+            <div className={classes.control}>
+                <label htmlFor="email">Email</label>
+                <input ref={emailInputRef} type="email" id="email" required />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="password">Password</label>
+                <input ref={passwordInputRef} type="password" id="password" required />
+            </div>
+            <button type="button" className={classes.toggle} onClick={switchAuthModeHandler} />
+        </form>
+    </section>
+  );
 };
 
 export default AuthForm;
--- client/budget-planner-web/src/components/Profile/UserProfile.js
+++ client/budget-planner-web/src/components/Profile/UserProfile.js
@@ -1,10 +1,10 @@
 const UserProfile = () => {
-  return (
-    <div>
-      <h1> user profile page </h1>
-      <p> here we'll render a form </p>
-    </div>
-  );
+    
+   <div>
+    <h1>
+        Your user profile
+    </h1>
+   </div>
 };
 
 export default UserProfile;
